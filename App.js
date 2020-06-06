41af7a6528ddf3d79db888c5b1a7b1aa71c2492a
taufiq <taufiq1689@gmail.com>
2020-06-07 00:30:59 +0800

    display sample stock finish

--- App.js
+++ App.js
@@ -7,116 +7,117 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Dimensions } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-
+import moment from 'moment';
+import { LineChart } from 'react-native-chart-kit';
 
 const alpha = require('alphavantage')({ key: 'MH4AW8511HB7KXGX' });
-const App: () => React$Node = () => {
-  alpha.data.intraday(`BBRI.JK`).then(data => {
-    console.log(data);
-  });
-  return (
 
-    
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+const chartConfig = {
+	backgroundGradientFrom: '#1E2923',
+	backgroundGradientFromOpacity: 0,
+	backgroundGradientTo: '#08130D',
+	backgroundGradientToOpacity: 0.5,
+	color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+	strokeWidth: 2, // optional, default 3
+	barPercentage: 0.5,
+	useShadowColorFromDataset: false // optional
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+class App extends React.Component {
+	state = {
+		stocks: {}
+	};
+	getStocksData = async () => {
+		let stocks = {};
+		let temp = {};
+
+    temp = await alpha.data.intraday(`BBRI.JK`, "full", null, '5min');
+    
+		let timeSeriesStocks = temp['Time Series (5min)'];
+    console.warn(timeSeriesStocks);
+    
+		temp = {};
+		temp['label'] = [];
+		temp['data'] = [];
+
+    let i = 0;
+    
+    let x = Object.keys(timeSeriesStocks).length/4
+    let labels = Object.keys(timeSeriesStocks).reverse()
+    
+
+    
+    
+
+    labels.forEach(label => {
+      let date = moment(label)
+			let hour = date.hour() + ':00';
+      
+			let newLabel =  '';
+  		temp['label'].push(newLabel);
+			temp['data'].push(parseFloat(timeSeriesStocks[label]['4. close']));        
+      i += 1;
+      
+
+    });
+
+		stocks['BBRI'] = temp;
+
+		this.setState({ stocks: stocks });
+	};
+
+	componentDidMount() {
+    
+    this.getStocksData();
+	}
+
+	render() {
+		return (
+			<View>
+				<LineChart
+					data={{
+						labels: this.state.stocks['BBRI'] ? this.state.stocks['BBRI']['label'] : [ '1', '2', '3', '4' ],
+						datasets: [
+							{
+								data: this.state.stocks['BBRI']
+									? this.state.stocks['BBRI']['data']
+									: [ Math.random() * 100, Math.random() * 100, Math.random() * 100 ]
+							},
+						]
+					}}
+					width={Dimensions.get('window').width} // from react-native
+					height={220}
+					yAxisInterval={1} // optional, defaults to 1
+					chartConfig={{
+						backgroundColor: '#e26a00',
+						backgroundGradientFrom: '#fb8c00',
+						backgroundGradientTo: '#ffa726',
+						decimalPlaces: 2, // optional, defaults to 2dp
+						color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+						labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+						style: {
+							borderRadius: 0
+						},
+						propsForDots: {
+							r: '0',
+							strokeWidth: '0',
+							stroke: '#ffa726'
+            },
+            propsForBackgroundLines:{
+              strokeWidth:"0"
+            }
+					}}
+					bezier
+					style={{
+						margin: 8,
+						borderRadius: 16
+					}}
+				/>
+			</View>
+		);
+	}
+}
 
 export default App;
