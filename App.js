a6d11c4d17fa945b67345fd48e74382cb037582a
taufiq <taufiq1689@gmail.com>
2020-06-07 17:25:12 +0800

    finish stockchart view

--- App.js
+++ App.js
@@ -7,108 +7,137 @@
  */
 
 import React from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Dimensions } from 'react-native';
+import {
+	SafeAreaView,
+	StyleSheet,
+	ScrollView,
+	View,
+	Text,
+	StatusBar,
+	Dimensions,
+	FlatList,
+	RefreshControl,
+	RefreshControlBase
+} from 'react-native';
 
 import moment from 'moment';
 import { LineChart } from 'react-native-chart-kit';
 
 const alpha = require('alphavantage')({ key: '#YOURAPIKEY' });
 
-const chartConfig = {
-	backgroundGradientFrom: '#1E2923',
-	backgroundGradientFromOpacity: 0,
-	backgroundGradientTo: '#08130D',
-	backgroundGradientToOpacity: 0.5,
-	color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-	strokeWidth: 2, // optional, default 3
-	barPercentage: 0.5,
-	useShadowColorFromDataset: false // optional
-};
-
 class App extends React.Component {
 	state = {
-		stocks: {}
+		stocks: [],
+		indices: [ 'BBRI','BBCA','BBNI' ],
+		refreshing: true
 	};
 	getStocksData = async () => {
-		let stocks = {};
-		let temp = {};
+		let stocks = [];
 
-		temp = await alpha.data.intraday(`BBRI.JK`, 'full', null, '60min');
+		for (const index of this.state.indices) {
+			let temp = {};
 
-		let timeSeriesStocks = temp['Time Series (60min)'];
+			temp = await alpha.data.intraday(index + '.JK', 'full', null, '60min');
 
-		temp = {};
-		temp['label'] = [];
-		temp['data'] = [];
+			let timeSeriesStocks = temp['Time Series (60min)'];
 
-		let i = 0;
+			temp = {};
+			temp['index'] = index;
+			temp['label'] = [];
+			temp['data'] = [];
 
-		let x = Object.keys(timeSeriesStocks).length / 4;
-		let labels = Object.keys(timeSeriesStocks).reverse();
-		var dateNow = moment();
+			let i = 0;
 
-		labels = labels.filter((label) => {
-			var oneMonthBeforeLimit = moment(dateNow).subtract(1, 'M');
-			return moment(label).month() >= oneMonthBeforeLimit.month();
-		});
+			let x = Object.keys(timeSeriesStocks).length / 4;
+			let labels = Object.keys(timeSeriesStocks).reverse();
+			var dateNow = moment();
 
-		labels.forEach((label) => {
-			let date = moment(label).utcOffset('-0500').format('YYYY-MM-DD HH:mm');
-			
+			labels = labels.filter((label) => {
+				var oneMonthBeforeLimit = moment(dateNow).subtract(1, 'M');
+				return moment(label).month() >= oneMonthBeforeLimit.month();
+			});
 
-			let newLabel = i == 0 || i == labels.length - 30 ? date : '';
-			temp['label'].push(newLabel);
-			temp['data'].push(parseFloat(timeSeriesStocks[label]['4. close']));
-			i += 1;
-		});
+			labels.forEach((label) => {
+				let date = moment(label).utcOffset('-0500').format('YYYY-MM-DD HH:mm');
 
-		stocks['BBRI'] = temp;
+				let newLabel = i == 0 || i == labels.length - 30 ? date : '';
+				temp['label'].push(newLabel);
+				temp['data'].push(parseFloat(timeSeriesStocks[label]['4. close']));
+				i += 1;
+			});
 
-		this.setState({ stocks: stocks });
+			stocks.push(temp);
+		}
+
+		this.setState({ stocks: stocks, refreshing: false });
 	};
 
 	componentDidMount() {
 		this.getStocksData();
 	}
 
+	onRefresh() {
+		// this.getStocksData()
+	}
+
 	render() {
 		return (
 			<View>
-				<LineChart
-					data={{
-						labels: this.state.stocks['BBRI'] ? this.state.stocks['BBRI']['label'] : [ '1', '2', '3', '4' ],
-						datasets: [
-							{
-								data: this.state.stocks['BBRI']
-									? this.state.stocks['BBRI']['data']
-									: [ Math.random() * 100, Math.random() * 100, Math.random() * 100 ]
-							}
-						]
+				<FlatList
+					data={this.state.stocks}
+					refreshControl={
+						<RefreshControl
+							refreshing={this.state.refreshing}
+							onRefresh={this.onRefresh}
+							colors={[ '#6fb243' ]}
+						/>
+					}
+					initialNumToRender={1}
+					renderItem={({ item: stock }) => {
+
+						return (
+							<View style={{ padding: 15 }}>
+								<Text style={{ fontSize: 30, paddingLeft: 5 }}>{stock['index']}</Text>
+								<LineChart
+									data={{
+										labels: stock ? stock['label'] : [ '0', '0', '0' ],
+										datasets: [
+											{
+												data: stock ? stock['data'] : [ 0, 0, 0 ]
+											}
+										]
+									}}
+									width={Dimensions.get('window').width - 20} // from react-native
+									height={220}
+									yAxisInterval={1} // optional, defaults to 1
+									chartConfig={{
+										backgroundColor: '#e26a00',
+										backgroundGradientFrom: '#fb8c00',
+										backgroundGradientTo: '#ffa726',
+										decimalPlaces: 2, // optional, defaults to 2dp
+										color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+										labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+										propsForDots: {
+											r: '0',
+											strokeWidth: '0',
+											stroke: '#ffa726'
+										},
+										propsForBackgroundLines: {
+											strokeWidth: '0'
+										}
+									}}
+									bezier
+									style={{
+										margin: 5,
+										borderRadius: 10
+									}}
+								/>
+							</View>
+						);
 					}}
-					width={Dimensions.get('window').width} // from react-native
-					height={220}
-					yAxisInterval={1} // optional, defaults to 1
-					chartConfig={{
-						backgroundColor: '#e26a00',
-						backgroundGradientFrom: '#fb8c00',
-						backgroundGradientTo: '#ffa726',
-						decimalPlaces: 2, // optional, defaults to 2dp
-						color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-						labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-						propsForDots: {
-							r: '0',
-							strokeWidth: '0',
-							stroke: '#ffa726'
-						},
-						propsForBackgroundLines: {
-							strokeWidth: '0'
-						}
-					}}
-					bezier
-					style={{
-						margin: 8,
-						borderRadius: 10
-					}}
+					keyExtractor={(item)=>item['index']}
+					onEndReachedThreshold={0.2}
+					contentContainerStyle={{ paddingBottom: 1 }}
 				/>
 			</View>
 		);
